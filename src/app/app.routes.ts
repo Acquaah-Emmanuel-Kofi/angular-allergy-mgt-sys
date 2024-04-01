a67c0753b9cd9c5c2004b8a70e969d095f4f5a5e
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-01 23:02:11 +0100

    Add record-allergy route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RecordAllergyComponent } from './pages/record-allergy/record-allergy.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LayoutComponent }, // Default route
+    { path: 'record-allergy', component: RecordAllergyComponent },
+  ];
