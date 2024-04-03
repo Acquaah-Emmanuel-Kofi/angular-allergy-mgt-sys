68d3d7c634d0d0ae1804044ca4e587e3257c06ca
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-03 11:30:31 +0000

    Update app routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
-import { LayoutComponent } from './components/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LayoutComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RecordAllergyComponent } from './pages/record-allergy/record-allergy.component';
-import { LayoutComponent } from './components/layout/layout.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    { path: '', component: LayoutComponent }, // Default route
+    { path: '', component: DashboardComponent }, // Default route
     { path: 'record-allergy', component: RecordAllergyComponent },
+    { path: 'history', component: HistoryComponent },
   ];
