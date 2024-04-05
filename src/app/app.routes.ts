83761adaa84032991be6fd9f97031285dde8f1a9
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-05 16:02:18 +0000

    Update routes with ProfileComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { RecordAllergyComponent } from './pages/record-allergy/record-allergy.component';
 import { HistoryComponent } from './pages/history/history.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
     { path: '', component: DashboardComponent }, // Default route
     { path: 'dashboard', component: DashboardComponent }, // Default route
     { path: 'record-allergy', component: RecordAllergyComponent },
     { path: 'history', component: HistoryComponent },
+    { path: 'profile', component: ProfileComponent },
   ];
