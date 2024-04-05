282076b0e837e203c2d8b552b49eccb33217fc07
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-05 16:06:55 +0000

    Update layout

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ToasterComponent } from '../toaster/toaster.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports:[ RouterLink, RouterLinkActive ],
+  imports:[ RouterLink, RouterLinkActive, ToasterComponent ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
