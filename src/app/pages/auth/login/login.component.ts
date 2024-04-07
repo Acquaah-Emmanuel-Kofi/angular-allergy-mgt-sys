8670b2aa0088af2173b91b330cd68a3c6a9d87cc
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-07 09:44:17 +0000

    Create Login Components

---
+++ src/app/pages/auth/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  constructor(public router: Router) {}
+
+  navigateToDashboard() {
+    this.router.navigate(['/dashboard']);
+  }
+}
