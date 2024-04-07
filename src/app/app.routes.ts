6cb9075ccf743964a12aca00ae06be5921872728
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-07 09:43:57 +0000

    Update routes with auth components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { RecordAllergyComponent } from './pages/record-allergy/record-allergy.co
 import { HistoryComponent } from './pages/history/history.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 export const routes: Routes = [
-    { path: '', component: DashboardComponent }, // Default route
+    { path: '', component: LandingPageComponent }, // Default route
     { path: 'dashboard', component: DashboardComponent }, // Default route
     { path: 'record-allergy', component: RecordAllergyComponent },
     { path: 'history', component: HistoryComponent },
     { path: 'profile', component: ProfileComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
   ];
