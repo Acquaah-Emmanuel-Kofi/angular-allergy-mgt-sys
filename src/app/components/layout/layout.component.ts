98a095cc99a7ea678301f9f48db3b66308975d76
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-08 10:12:39 +0000

    Add theme functionality

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ToasterComponent } from '../toaster/toaster.component';
+import { ThemeService } from '../../services/theme/theme.service';
 
 @Component({
   selector: 'app-layout',
@@ -12,4 +13,18 @@ import { ToasterComponent } from '../toaster/toaster.component';
 export class LayoutComponent {
     appName = 'Aller Gus';
 
+    
+  constructor(public themeService: ThemeService) { }
+
+  ngOnInit(): void {
+    // Change the icons inside the button based on previous settings
+    const colorTheme = localStorage.getItem('color-theme');
+    if (colorTheme === 'dark' || (!colorTheme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      this.themeService.darkTheme = true;
+    }
+  }
+
+  toggleTheme() {
+    this.themeService.toggleTheme();
+  }
 }
