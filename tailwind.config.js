2a5896f110dcc82d1ab5c63244727635e6cf6f85
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-08 11:45:35 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{html,ts}",
     "./node_modules/flowbite/**/*.js"
@@ -9,7 +10,14 @@ module.exports = {
       backgroundImage: {
         'history-card-bg': "url('/assets/images/history-card-bg.jpg')",
         'facts-bg': "url('/assets/images/patreon.jpeg')",
-      }
+        'landing-bg': "url('/assets/images/landing-bg.webp')"
+      },
+      fontSize: {
+        'sm': '14px',
+      },
+      width: {
+        'half-width': "50%",
+      },
     },
   },
   plugins: [
