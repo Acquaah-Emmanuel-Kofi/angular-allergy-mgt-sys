f0dff78229779357a0c765839bed69a67d968f5e
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-09 22:39:57 +0000

    Add toaster and app-layout to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { LayoutComponent } from './components/layout/layout.component';
+import { ToasterComponent } from './components/toaster/toaster.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LayoutComponent],
+  imports: [RouterOutlet, LayoutComponent, ToasterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
+
+  constructor(private router: Router) {}
+
+  isAuthenticated(): boolean {
+    const authRoutes = ['/login', '/register']; 
+    const currentRoute = this.router.url;
+    return authRoutes.includes(currentRoute) || currentRoute === '/';
+  }
   
   ngOnInit(): void {
     initFlowbite();
