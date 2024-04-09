5f5344451e91d9707c7295d8167f38f07489371f
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-09 08:50:39 +0000

    Create allergies interface

---
+++ src/app/interfaces/allergies.ts
@@ -0,0 +1,12 @@
+export interface AllergyFact {
+    id: number;
+    title: string;
+    fact: string;
+}
+
+export interface History {
+  id: number;
+  title: string;
+  description: string;
+  date: string;
+}
