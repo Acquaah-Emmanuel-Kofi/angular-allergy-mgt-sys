4fd3848e33bc5c6ddcb1a95349e652ad8d5e91ab
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-10 11:04:13 +0000

    Add reactive from controls and functionalities to login component

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,18 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { LogoComponent } from '../../../shared/logo/logo.component';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, LogoComponent],
+  imports: [RouterLink, LogoComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  constructor(public router: Router) {}
+  constructor(public router: Router, private formBuilder: FormBuilder) {}
 
-  navigateToDashboard() {
-    this.router.navigate(['/dashboard']);
+  isLoading = signal(false);
+
+  form = this.formBuilder.group({
+    email: [
+      '',
+      {
+        validators: [Validators.required, Validators.email],
+        updateOn: 'blur',
+      },
+    ],
+    password: ['', [Validators.required]],
+  });
+
+  get email() {
+    return this.form.controls['email'];
+  }
+
+  get password() {
+    return this.form.controls['password'];
+  }
+
+  login() {
+    this.isLoading.set(true);
+    console.log(this.form.value);
+
+    // this.router.navigate(['/dashboard']);
   }
 }
