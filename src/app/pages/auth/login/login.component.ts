6d5425890199a3b412ac3e2d3ed688a7be18d29a
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-10 20:39:50 +0000

    Update login with api calls

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -8,46 +8,94 @@ import {
   Validators,
 } from '@angular/forms';
 import { AlertComponent } from '../../../components/alert/alert.component';
+import { checkUsernameValidator } from '../../../utility/validators/auth/username.validator';
+import { ToasterService } from '../../../components/toaster/toaster.service';
+import { AuthenticationService } from '../../../services/auth/authentication.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, LogoComponent, FormsModule, ReactiveFormsModule, AlertComponent],
+  imports: [
+    RouterLink,
+    LogoComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    AlertComponent,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  constructor(public router: Router, private formBuilder: FormBuilder) {}
+  constructor(
+    private _router: Router,
+    private _formBuilder: FormBuilder,
+    private _toast: ToasterService,
+    private _authService: AuthenticationService
+  ) {}
 
   formIsValid = signal(true);
   isLoading = signal(false);
-  errorMessage = signal("");
+  errorMessage = signal('');
 
-  form = this.formBuilder.group({
-    email: [
+  form = this._formBuilder.group({
+    username: [
       '',
       {
-        validators: [Validators.required, Validators.email],
+        validators: [Validators.required, checkUsernameValidator()],
         updateOn: 'blur',
       },
     ],
     password: ['', [Validators.required]],
   });
 
-  get email() {
-    return this.form.controls['email'];
+  get username() {
+    return this.form.controls['username'];
   }
 
   get password() {
     return this.form.controls['password'];
   }
 
-  login() {
-    this.formIsValid.set(false);
-    this.errorMessage.set("Invalid Credentials");
-    this.isLoading.set(true);
-    console.log(this.form.value);
+  checkFormValidity() {
+    if (!this.form.valid) {
+      this.formIsValid.set(false);
 
-    // this.router.navigate(['/dashboard']);
+      setTimeout(() => {
+        this.formIsValid.set(true);
+      }, 3000);
+
+      this.errorMessage.set('Invalid credentials!');
+    }
+  }
+
+  login() {
+    this.checkFormValidity();
+
+    let formData = this.form.value;
+
+    if (this.form.valid) {
+      this.isLoading.set(true);
+
+      this._authService.loginUser(formData).subscribe({
+        next: (response) => {
+          this.isLoading.set(false);
+
+          if (response.status === 'success') {
+            this.form.reset();
+            this._toast.showSuccess('Login Sucessful!');
+            this._authService.saveUsefulDetails(response);
+            this._router.navigate(['/dashboard']);
+          } else {
+            this._toast.showError('User does not exists!');
+          }
+        },
+        error: () => {
+          this.isLoading.set(false);
+          this._toast.showError(
+            'Something went wrong! Please, check your internet and try again.'
+          );
+        },
+      });
+    }
   }
 }
