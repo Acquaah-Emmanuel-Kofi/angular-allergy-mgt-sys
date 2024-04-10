80aebdf322234a214edb2489eb23336147218ce7
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-10 20:41:49 +0000

    Update historycard.component.ts

--- src/app/components/historycard/historycard.component.ts
+++ src/app/components/historycard/historycard.component.ts
@@ -1,15 +1,14 @@
 import { Component, Input } from '@angular/core';
-import { History } from '../../interfaces/allergies';
+import { History } from '../../interfaces/allergies.interface';
 
 @Component({
   selector: 'app-historycard',
   standalone: true,
   imports: [],
   templateUrl: './historycard.component.html',
-  styleUrl: './historycard.component.scss'
+  styleUrl: './historycard.component.scss',
 })
 export class HistorycardComponent {
-
   @Input()
   historyData!: History;
 }
