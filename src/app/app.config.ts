a48ec59f6cb7543011c8dea6456ee48caf605c99
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-11 20:26:38 +0000

    Update app.config.ts with interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,15 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth/auth.interceptor';
+import { headersInterceptor } from './interceptors/headers/headers.interceptor';
+import { errorInterceptor } from './interceptors/error/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [provideRouter(routes), provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([authInterceptor, headersInterceptor, errorInterceptor]),
+    )
+  ]
 };
