d53049ca4c0c8f85aa88577d58a3edb4214a2dc7
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-11 20:26:11 +0000

    Take profile form inputs to update

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { checkUsernameValidator } from '../../utility/validators/auth/username.validator';
+import { AuthenticationService } from '../../services/auth/authentication.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
+  constructor(private _formBuilder: FormBuilder, private _authService: AuthenticationService) {}
 
+  userId = this._authService.getUserDeatils()?.userId;
+
+  form = this._formBuilder.group({
+    firstname: [''],
+    lastname: [''],
+    dateOfBirth: [''],
+    gender: [''],
+    username: ['', checkUsernameValidator()],
+    email: ['', Validators.email],
+  });
+
+  updateProfile() {    
+    if(this.userId) {
+      this._authService.updateUserDetails(this.userId, this.form.value).subscribe({
+        next: (response) => {
+          console.log(response);
+          
+        }, error: (error) => {
+          console.log(error);
+          
+        }
+      });
+    }
+  }
 }
