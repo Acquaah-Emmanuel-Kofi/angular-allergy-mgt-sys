d3f10cbc453a9eff4a635914aafe8ffa1161d5c1
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-12 10:05:21 +0000

    Update toaster.component.ts

--- src/app/components/toaster/toaster.component.ts
+++ src/app/components/toaster/toaster.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { ToasterService } from './toaster.service';
+import { ToasterService } from './services/toaster.service';
 
 @Component({
   selector: 'app-toaster',
   standalone: true,
   imports: [],
   templateUrl: './toaster.component.html',
-  styleUrl: './toaster.component.scss'
+  styleUrl: './toaster.component.scss',
 })
 export class ToasterComponent {
-
   constructor(public toaster: ToasterService) {}
-
 }
