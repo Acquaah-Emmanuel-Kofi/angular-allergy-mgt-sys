30f013d9908ccb6cd83b94b632a71b208daf6566
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-12 18:36:12 +0000

    Implement form validation and update profile

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit, signal } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { checkUsernameValidator } from '../../utility/validators/auth/username.validator';
 import { AuthenticationService } from '../../services/auth/authentication.service';
+import { ToasterService } from '../../components/toaster/services/toaster.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,10 +16,18 @@ import { AuthenticationService } from '../../services/auth/authentication.servic
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {
-  constructor(private _formBuilder: FormBuilder, private _authService: AuthenticationService) {}
+export class ProfileComponent implements OnInit {
+  isLoading = signal(false);
 
-  userId = this._authService.getUserDeatils()?.userId;
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _authService: AuthenticationService,
+    private _toast: ToasterService
+  ) {}
+
+  ngOnInit(): void {
+    this.fetchUserDetails();
+  }
 
   form = this._formBuilder.group({
     firstname: [''],
@@ -24,17 +38,58 @@ export class ProfileComponent {
     email: ['', Validators.email],
   });
 
-  updateProfile() {    
-    if(this.userId) {
-      this._authService.updateUserDetails(this.userId, this.form.value).subscribe({
+  get username() {
+    return this.form.controls['username'];
+  }
+
+  get email() {
+    return this.form.controls['email'];
+  }
+
+  updateProfile() {
+    if (this.checkFormValidity()) {
+      this.isLoading.set(true);
+
+      this._authService.updateUserDetails(this.form.value).subscribe({
         next: (response) => {
-          console.log(response);
-          
-        }, error: (error) => {
-          console.log(error);
-          
-        }
+          this.isLoading.set(false);
+
+          if (response.status === 'success') {
+            this.form.reset();
+            this._toast.showSuccess('Update Sucessful!');
+            this.fetchUserDetails();
+          } else {
+            this._toast.showError('Update Failed!');
+          }
+        },
+        error: () => {
+          this.isLoading.set(false);
+          this._toast.showError(
+            'Something went wrong. Please, check your internet connection and try again!'
+          );
+        },
       });
     }
+  }
+
+  checkFormValidity(): Boolean {
+    if (!this.form.value) {
+      this._toast.showError('All fields cannot be empty.');
+
+      return false;
+    }
+
+    return true;
+  }
+
+  fetchUserDetails() {
+    this._authService.getUserProfileDetails().subscribe({
+      next: (details) => {
+        this.form.setValue(details);
+      },
+      error: (error) => {
+        this._toast.showError(error);
+      },
+    });
   }
 }
