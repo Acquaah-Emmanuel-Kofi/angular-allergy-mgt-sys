6bd6e7d84b6ddb91eb163da6de6889d69876026c
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-12 18:24:33 +0000

    Update error message in errror inteceptor

--- src/app/interceptors/error/error.interceptor.ts
+++ src/app/interceptors/error/error.interceptor.ts
@@ -12,11 +12,8 @@ export const errorInterceptor: HttpInterceptorFn = (
 ) => {
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
-      // Handle the error here
-      console.error('HTTP error occurred:', error);
-      // You can throw a custom error here or handle different error scenarios based on the status code
       return throwError(
-        () => new Error('HTTP error occurred. Customized Error')
+        () => new Error("HTTP error occurred! Check your internet connection.")
       );
     })
   );
