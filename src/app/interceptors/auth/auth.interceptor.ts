ddb2d5cd1d435728f1e582c7dac159f54ec9c26a
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-20 09:36:28 +0000

    Implement token session time to log out a user

--- src/app/interceptors/auth/auth.interceptor.ts
+++ src/app/interceptors/auth/auth.interceptor.ts
@@ -5,15 +5,37 @@ import {
 } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthenticationService } from '../../services/auth/authentication.service';
+import { DecodedToken } from '../../interfaces/decodeJwt.interface';
+import { decodeJwt } from '../../utility/constants/auth.constants';
+import { Router } from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (
   req: HttpRequest<any>,
   next: HttpHandlerFn
 ) => {
-  const authToken = inject(AuthenticationService).getAccessToken();
+  let router = inject(Router);
+  let authService = inject(AuthenticationService);
+  const authToken = authService.getAccessToken();
 
   // If an access token is available, add it to the request headers
   if (authToken) {
+    // Decode the JWT token
+    let decodedToken: DecodedToken | null = decodeJwt(authToken);
+
+    // Access the data from the payload
+    if (decodedToken && decodedToken.payload) {
+      const payloadData = decodedToken.payload;
+      const isExpired =
+        payloadData && payloadData.exp
+          ? payloadData.exp < Date.now() / 1000
+          : false;
+
+      if (isExpired) {
+        authService.logoutUser();
+      }
+    } else {
+      router.navigateByUrl('/login');
+    }
     // Clone the request and add the authorization header
     const authReq = req.clone({
       setHeaders: {
