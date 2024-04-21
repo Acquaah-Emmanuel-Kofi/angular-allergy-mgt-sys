5524bc95efaa54c908325afd645d81e538d15cff
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-21 15:27:31 +0000

    Update form-stepper2 and get form data

--- src/app/pages/record-allergy/form-step2/form-step2.component.ts
+++ src/app/pages/record-allergy/form-step2/form-step2.component.ts
@@ -1,48 +1,27 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FormData } from '../../../interfaces/allergies.interface';
 
 @Component({
   selector: 'app-form-step2',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule],
+  imports: [FormsModule],
   templateUrl: './form-step2.component.html',
-  styleUrl: './form-step2.component.scss'
+  styleUrl: './form-step2.component.scss',
 })
-export class FormStep2Component implements OnInit {
+export class FormStep2Component {
   @Output() nextStepForm: EventEmitter<any> = new EventEmitter();
   @Output() prevStepForm: EventEmitter<any> = new EventEmitter();
+  @Input() form!: FormData;
+  @Input() handleCheckboxChange!: (event: Event) => void;
 
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.draftFormData();
-  }
-
-  formStepper2 = this._formBuilder.group({
-    experiencedSymptoms: [''],
-    symptomsDuration: ['', Validators.required],
-    symptomsSeverity: ['', Validators.required],
-  });
-
-  // Method TO send form data to the next step form
   nextStep(): void {
-    // Emit the FormGroup containing the form data
-    this.nextStepForm.emit(this.formStepper2.value);
+    this.nextStepForm.emit();
   }
 
-  prevStep( ){
+  prevStep() {
     this.prevStepForm.emit();
-  }
-
-  draftFormData(): void {
-    const data = localStorage.getItem('STEP_2');
-
-    if (data) {
-      this.formStepper2.setValue(JSON.parse(data));
-    }
-
-    this.formStepper2.valueChanges.subscribe((value) =>
-      localStorage.setItem('STEP_2', JSON.stringify(value))
-    );
   }
 }
