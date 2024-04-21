64596ae35eda27977f1535b01dca00502454c838
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-21 15:38:15 +0000

    Update form data interface

--- src/app/interfaces/allergies.interface.ts
+++ src/app/interfaces/allergies.interface.ts
@@ -10,3 +10,16 @@ export interface History {
   description: string;
   date: string;
 }
+
+export interface FormData {
+  medicalCondition: string;
+  medicalConditionExplained: string;
+  onMedication: string;
+  allergySymptoms: string;
+  symptomSeverity: string;
+  causeOfSymptom: string;
+  timesOfReaction: string;
+  allergyExposedTo: string;
+  doesTheUserSmoke: string;
+  additionalNotes: string;
+}
