7595a59df046714d509a7ee63f09fcd45174606b
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-21 15:40:54 +0000

    Use crypto js for encryption and decryption

--- src/app/utility/constants/auth.constants.ts
+++ src/app/utility/constants/auth.constants.ts
@@ -1,43 +1,41 @@
 import * as CryptoJS from 'crypto-js';
 import { DecodedToken } from '../../interfaces/decodeJwt.interface';
+import { environment } from '../../../environments/environment.development';
 
-export const ACCESS_TOKEN_KEY: string = 'TOKEN';
+export const ACCESS_TOKEN_KEY: string = 'ALLERGY_ACCESS_KEY';
 export const USERID_KEY: string = 'USER_ID';
 export const USERNAME_KEY: string = 'USERNAME';
 
-const SECRET_KEY: string =
-  'jdyYCUh04EBwVC50DXiak95NrCRt0NgFbOru9ejF4u7ipbySterYrpqUFMikY79ObfxlOKAxBDUHWaZO0j3tnxpNnqgNxpSjwsifUiaEZOeSPLhLr7Uv1dCfw9h7sePrCOWRZPPlkCujlhNvIyrb1PwgMC7rjUowdALVevfgSsTJYz7Dd7B3bvi4gO62nOBvZ6XXWfo838IuyYT2IiunYvXkABnlu0wkxT0K7pEKUV2R4hiCzMZVSrsA93JgLmdd';
 
-
-// Encrypt data using sjcl library
+// Encrypt data using CryptoJS library
 export function encryptData(data: string): string {
-  return CryptoJS.AES.encrypt(data, SECRET_KEY).toString();
+  return CryptoJS.AES.encrypt(data, environment.SECRET_KEY).toString();
 }
 
-// Decrypt data using sjcl library
+// Decrypt data using CryptoJS library
 export function decryptData(data: string): string {
-  const bytes = CryptoJS.AES.decrypt(data, SECRET_KEY);
+  const bytes = CryptoJS.AES.decrypt(data, environment.SECRET_KEY);
   return bytes.toString(CryptoJS.enc.Utf8);
 }
 
 export function decodeJwt(token: String): DecodedToken | null {
-    // Check if the token is not empty
-    if (!token) {
-        return null;
-    }
+  // Check if the token is not empty
+  if (!token) {
+    return null;
+  }
 
-    // Split the token into its three parts: header, payload, and signature
-    const parts = token.split('.');
-    if (parts.length !== 3) {
-        // Invalid token format
-        return null;
-    }
+  // Split the token into its three parts: header, payload, and signature
+  const parts = token.split('.');
+  if (parts.length !== 3) {
+    // Invalid token format
+    return null;
+  }
 
-    // Decode and parse the header, payload, and signature
-    const header = JSON.parse(atob(parts[0]));
-    const payload = JSON.parse(atob(parts[1]));
-    const signature = parts[2];
+  // Decode and parse the header, payload, and signature
+  const header = JSON.parse(atob(parts[0]));
+  const payload = JSON.parse(atob(parts[1]));
+  const signature = parts[2];
 
-    // Return an object containing the decoded header, payload, and signature
-    return { header, payload, signature };
+  // Return an object containing the decoded header, payload, and signature
+  return { header, payload, signature };
 }
