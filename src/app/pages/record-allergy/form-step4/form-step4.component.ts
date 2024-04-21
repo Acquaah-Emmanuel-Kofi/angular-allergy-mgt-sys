0fe77bfad19bdab8c055ff82622bb7a4645d6156
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-21 15:28:17 +0000

    Update form-stepper4 and get form data

--- src/app/pages/record-allergy/form-step4/form-step4.component.ts
+++ src/app/pages/record-allergy/form-step4/form-step4.component.ts
@@ -1,48 +1,26 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FormData } from '../../../interfaces/allergies.interface';
 
 @Component({
   selector: 'app-form-step4',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule],
+  imports: [FormsModule],
   templateUrl: './form-step4.component.html',
-  styleUrl: './form-step4.component.scss'
+  styleUrl: './form-step4.component.scss',
 })
-export class FormStep4Component implements OnInit {
-  @Output() nextStepForm: EventEmitter<any> = new EventEmitter();
+export class FormStep4Component {
+  @Output() submitForm: EventEmitter<any> = new EventEmitter();
   @Output() prevStepForm: EventEmitter<any> = new EventEmitter();
+  @Input() form!: FormData;
 
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.draftFormData();
-  }
-
-  formStepper4 = this._formBuilder.group({
-    occupationalOrEnvironmentalAllergens: ['', Validators.required],
-    exposedToSmoke: ['', Validators.required],
-    additionalInformation: ['', Validators.required],
-  });
-
-  // Method TO send form data to the next step form
   nextStep(): void {
-    // Emit the FormGroup containing the form data
-    this.nextStepForm.emit(this.formStepper4.value);
+    this.submitForm.emit();
   }
 
   prevStep() {
     this.prevStepForm.emit();
-  }
-
-  draftFormData(): void {
-    const data = localStorage.getItem('STEP_4');
-
-    if (data) {
-      this.formStepper4.setValue(JSON.parse(data));
-    }
-
-    this.formStepper4.valueChanges.subscribe((value) =>
-      localStorage.setItem('STEP_4', JSON.stringify(value))
-    );
   }
 }
