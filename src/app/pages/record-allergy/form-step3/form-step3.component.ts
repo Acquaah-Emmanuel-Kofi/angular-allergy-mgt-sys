8b6998df9b67932a3db14c0507d2ee8850cd353f
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-21 15:27:59 +0000

    Update form-stepper3 and get form data

--- src/app/pages/record-allergy/form-step3/form-step3.component.ts
+++ src/app/pages/record-allergy/form-step3/form-step3.component.ts
@@ -1,49 +1,27 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FormData } from '../../../interfaces/allergies.interface';
 
 @Component({
   selector: 'app-form-step3',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule],
+  imports: [FormsModule],
   templateUrl: './form-step3.component.html',
   styleUrl: './form-step3.component.scss',
 })
-export class FormStep3Component implements OnInit {
+export class FormStep3Component {
   @Output() nextStepForm: EventEmitter<any> = new EventEmitter();
   @Output() prevStepForm: EventEmitter<any> = new EventEmitter();
+  @Input() form!: FormData;
+  @Input() handleCheckboxChange!: (event: Event) => void;
 
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.draftFormData();
-  }
-
-  formStepper3 = this._formBuilder.group({
-    whenSymptomsOccur: ['', Validators.required],
-    symptomsSeverity: ['', Validators.required],
-    timesOfReaction: ['', Validators.required],
-    medicationsOrTreatments: ['', Validators.required]
-  });
-
-  // Method TO send form data to the next step form
   nextStep(): void {
-    // Emit the FormGroup containing the form data
-    this.nextStepForm.emit(this.formStepper3.value);
+    this.nextStepForm.emit();
   }
 
   prevStep() {
     this.prevStepForm.emit();
-  }
-
-  draftFormData(): void {
-    const data = localStorage.getItem('STEP_3');
-
-    if (data) {
-      this.formStepper3.setValue(JSON.parse(data));
-    }
-
-    this.formStepper3.valueChanges.subscribe((value) =>
-      localStorage.setItem('STEP_3', JSON.stringify(value))
-    );
   }
 }
