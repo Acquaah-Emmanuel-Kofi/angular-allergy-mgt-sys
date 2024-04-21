d20dbfbc02246f43f54ef5ea65b3d58074f00e69
waglarry <emmanuelacquaah2468@gmail.com>
2024-04-21 15:27:06 +0000

    Update form-stepper1 and get form data

--- src/app/pages/record-allergy/form-step1/form-step1.component.ts
+++ src/app/pages/record-allergy/form-step1/form-step1.component.ts
@@ -1,48 +1,33 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import {
-  FormBuilder,
   FormsModule,
-  ReactiveFormsModule,
-  Validators,
 } from '@angular/forms';
+import { FormData } from '../../../interfaces/allergies.interface';
 
 @Component({
   selector: 'app-form-step1',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule],
+  imports: [FormsModule],
   templateUrl: './form-step1.component.html',
   styleUrl: './form-step1.component.scss',
 })
-export class FormStep1Component implements OnInit {
+export class FormStep1Component {
   @Output() nextStepForm: EventEmitter<any> = new EventEmitter();
+  @Input() form!: FormData;
 
-  constructor(private _formBuilder: FormBuilder) {}
+  isChecked: boolean = false;
 
-  ngOnInit(): void {
-    this.draftFormData();
-  }
+  constructor() {}
 
-  formStepper1 = this._formBuilder.group({
-    allergicReactionsExperienced: ['', Validators.required],
-    hasExistingMedicalCondition: ['', Validators.required],
-    takingMedication: ['', Validators.required],
-  });
-
-  // Method TO send form data to the next step form
   nextStep(): void {
-    // Emit the FormGroup containing the form data
-    this.nextStepForm.emit(this.formStepper1.value);
+    this.nextStepForm.emit();
   }
 
-  draftFormData(): void {
-    const data = localStorage.getItem('STEP_1');
+  onChangeYes() {
+    this.isChecked = true;
+  }
 
-    if (data) {
-      this.formStepper1.setValue(JSON.parse(data));
-    }
-
-    this.formStepper1.valueChanges.subscribe((value) =>
-      localStorage.setItem('STEP_1', JSON.stringify(value))
-    );
+  onChangeNo() {
+    this.isChecked = false;
   }
 }
