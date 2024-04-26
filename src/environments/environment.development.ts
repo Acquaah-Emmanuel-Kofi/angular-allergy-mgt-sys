f05cbfbfb8d66c73850061cdb8c356174d73e28b
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-04-26 15:09:30 +0000

    updated api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,5 @@
 export const environment = {
     BACKEND_API_BASE_URL: 'https://allergy-u6fk.onrender.com/api/v1',
+    NGROK_API_BASE_URL: 'https://2499-196-61-44-226.ngrok-free.app/api/v1',
+    SECRET_KEY: 'jdyYCUh04EBwVC50DXiak95NrCRt0NgFbOru9ejF4u7ipbySterYrpqUFMikY79ObfxlOKAxBDUHWaZO0j3tnxpNnqgNxpSjwsifUiaEZOeSPLhLr7Uv1dCfw9h7sePrCOWRZPPlkCujlhNvIyrb1PwgMC7rjUowdALVevfgSsTJYz7Dd7B3bvi4gO62nOBvZ6XXWfo838IuyYT2IiunYvXkABnlu0wkxT0K7pEKUV2R4hiCzMZVSrsA93JgLmdd',
 };
