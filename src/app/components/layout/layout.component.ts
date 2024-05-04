14ed6df0282c628a0a10d96d7c0b2321b8188668
waglarry <emmanuelacquaah2468@gmail.com>
2024-05-04 06:52:16 +0000

    update navbar profile with user details

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -7,22 +7,39 @@ import { AuthenticationService } from '../../services/auth/authentication.servic
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports:[ LogoComponent ,RouterLink, RouterLinkActive ],
+  imports: [LogoComponent, RouterLink, RouterLinkActive],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+  styleUrl: './layout.component.scss',
 })
 export class LayoutComponent {
-    appName = 'Aller Gus';
+  appName = 'Aller Gus';
 
-    
-  constructor(public _themeService: ThemeService, private _authService: AuthenticationService) { }
+  constructor(
+    public _themeService: ThemeService,
+    private _authService: AuthenticationService
+  ) {}
+
+  username: string | null = null;
+  email: string | null = null;
 
   ngOnInit(): void {
+    this.fetchUserDetails();
+
     const colorTheme = localStorage.getItem('color-theme');
-    if(colorTheme === 'dark' || (!colorTheme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    if (
+      colorTheme === 'dark' ||
+      (!colorTheme && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
       document.documentElement.classList.toggle('dark', true);
       this.toggleTheme();
     }
+  }
+
+  fetchUserDetails() {
+    this._authService.getUserProfileDetails().subscribe((details) => {
+      this.username = details.username;
+      this.email = details.email;
+    });
   }
 
   toggleTheme() {
