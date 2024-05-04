c4ee1eb116cb547ba798fefa01bb5f0e12ad289e
waglarry <emmanuelacquaah2468@gmail.com>
2024-05-04 16:55:35 +0000

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  BACKEND_API_BASE_URL: process.env?.['BACKEND_API_BASE_URL'],
+  NGROK_API_BASE_URL: process.env?.['NGROK_API_BASE_URL'],
+  SECRET_KEY: process.env?.['SECRET_KEY']
+};
